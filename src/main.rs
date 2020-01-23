0519f5696e131c1fa5501b6891d6f6cb59f61ca9
zhxiaog <zhxiaog@outlook.com>
2020-01-23 12:00:19 +0800

    add cmdline parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use std::env;
+use getopts::{Options,Matches};
+
 fn main() {
-    println!("Hello, world!");
+  let args: Vec<String> = env::args().collect();
+    let program = args[0].clone();
+
+    let mut opts = Options::new();
+    opts.reqopt("t", "", "set app token", "APP_TOKEN");
+    opts.optflag("h", "help", "print this help");
+    match opts.parse(&args[1..]) {
+        Ok(m) if m.opt_present("h")=> print_usage(&program, opts),
+        // TOOD: print failure reason
+        Err(f) => print_usage(&program, opts), 
+        Ok(m) => start(m)
+    }
+}
+
+fn start(matches:Matches) {
+    if let Some(token) = matches.opt_str("t") {
+      println!("starting schedules runner with token: {:?}", token);
+    }
+}
+
+fn print_usage(program: &str, opts: Options) {
+  let brief = format!("Usage: {} [options] -t APP_TOKEN", program);
+  print!("{}", opts.usage(&brief));
 }
